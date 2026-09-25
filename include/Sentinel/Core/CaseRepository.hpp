03a58d4843db39df6dbac2d0776731471b722fd8
afterburn25 <afterburn32@gmail.com>
2026-09-25 12:03:13 -0500

    Enable encrypted case repository fields

--- include/Sentinel/Core/CaseRepository.hpp
+++ include/Sentinel/Core/CaseRepository.hpp
@@ -1,6 +1,28 @@
 #pragma once
 #include "Sentinel/Core/Case.hpp"
+#include "Sentinel/Security/Crypto.hpp"
+#include "Sentinel/Security/KeyManager.hpp"
 #include "Sentinel/Storage/SqliteDatabase.hpp"
+
 namespace sentinel {
-class SqliteCaseRepository final:public ICaseRepository{public:explicit SqliteCaseRepository(SqliteDatabase& db):db_(db){}void Insert(const CaseRecord&) override;std::optional<CaseRecord> Get(const CaseId&) override;std::vector<CaseRecord> List() override;void Update(const CaseRecord&) override;private:SqliteDatabase& db_;};
+
+class SqliteCaseRepository final : public ICaseRepository {
+public:
+    SqliteCaseRepository(
+        SqliteDatabase& db,
+        IKeyManager* keys = nullptr,
+        IAeadCipher* cipher = nullptr)
+        : db_(db), keys_(keys), cipher_(cipher) {}
+
+    void Insert(const CaseRecord&) override;
+    std::optional<CaseRecord> Get(const CaseId&) override;
+    std::vector<CaseRecord> List() override;
+    void Update(const CaseRecord&) override;
+
+private:
+    SqliteDatabase& db_;
+    IKeyManager* keys_{};
+    IAeadCipher* cipher_{};
+};
+
 }
